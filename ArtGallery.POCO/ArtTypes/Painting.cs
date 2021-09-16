efd4eac99ef681923a604686f3b12aa776972dcd
seth-d-mclaughlin <seth-d-mclaughlin@gmail.com>
2021-09-16 15:38:14 -0400

    Add project files.

---
+++ ArtGallery.POCO/ArtTypes/Painting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtGallery.POCO
+{
+    public class Painting : IArt
+    {
+        public Painting(string name, string artist, DateTime start, DateTime finish, double height, double width)
+        {
+            Name = name;
+            Artist = artist;
+            Start = start;
+            Finish = finish;
+            Height = height;
+            Width = width;
+        }
+
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        public int TimeToCreate
+        {
+            get
+            {
+                TimeSpan differenceInTime = Start - Finish;
+                int days = (int)differenceInTime.TotalDays;
+                return days;
+            }
+        }
+
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        
+    }
+}
---
+++ ArtGallery.POCO/IArt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtGallery.POCO
+{
+    public interface IArt
+    {
+        string Name { get; set; }
+        string Artist { get; set; }
+        DateTime Start { get; set; }
+        DateTime Finish { get; set; }
+        int TimeToCreate { get; }
+
+        
+    }
+}
